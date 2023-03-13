c36a3052c1586ab11f42f1627c6c19d30b889e68
Hamed Osama <77761661+HamedOsama@users.noreply.github.com>
2023-03-13 13:16:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
     const clientsFetch = async () => {
       let {
         data
-      } = await axios.get("https://api.tawyanoffice.com/api/v1/admin/clients", {
+      } = await axios.get("https://api.tawyanoffice.com/api/v1/admin/client", {
         Headers: {
           Accept: "/",
           "Content-Type": "application/json",
@@ -57,7 +57,7 @@ const App = () => {
     const blogsFetch = async () => {
       let {
         data
-      } = await axios.get("https://api.tawyanoffice.com/api/v1/admin/blogs", {
+      } = await axios.get("https://api.tawyanoffice.com/api/v1/admin/blog", {
         Headers: {
           Accept: "/",
           "Content-Type": "application/json",
@@ -72,7 +72,7 @@ const App = () => {
       let {
         data
       } = await axios.get(
-        "https://api.tawyanoffice.com/api/v1/admin/services",
+        "https://api.tawyanoffice.com/api/v1/admin/service",
         {
           Headers: {
             Accept: "/",
