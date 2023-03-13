5601cbe8a2671dc41df7cc2f536404ce489c2134
Hamed Osama <77761661+HamedOsama@users.noreply.github.com>
2023-03-13 13:47:00 +0200

    Update PrivateRoutes.js

--- src/utils/PrivateRoutes.js
+++ src/utils/PrivateRoutes.js
@@ -1,7 +1,9 @@
 import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoutes = ({ t }) => {
+  console.log(t)
   let auth = { token: t };
+  console.log(auth)
   return auth.token ? <Outlet /> : <Navigate to="/login" />;
 };
 
