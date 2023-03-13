9b5e058825cd179341653a2d93d0d92a247715a8
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2023-03-13 16:51:13 +0200

    1

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { redirect  } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
 
 axios.defaults.withCredentials = true;
 const Login = () => {
@@ -23,7 +23,7 @@ const Login = () => {
       .request(requestOptions)
       .then(() => {
         localStorage.setItem("token", true);
-        return redirect('/');
+        return Navigate('/');
       })
       .catch(err => {
         console.error(err);
